eae2ec73f623b9ad8bc2c60def21ba128197c636
Karly Kizewski <kizewski.karly@gmail.com>
2019-12-11 04:22:14 +0000

    rock creation spec test passes

--- app/controllers/rocks_controller.rb
+++ app/controllers/rocks_controller.rb
@@ -1,4 +1,19 @@
 class RocksController < ApplicationController
+  def new
+    @rock = Rock.new
+  end
+
   def index
+  end
+
+  def create
+    @rock = Rock.create(rock_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def rock_params
+    params.require(:rock).permit(:name, :address, :description, :message)
   end
 end
--- spec/controllers/rocks_controller_spec.rb
+++ spec/controllers/rocks_controller_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe RocksController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "rocks#create action" do 
+    it "should successfully create a new rock in the database" do
+      post :create, params: { rock: { name: "Rock" } }
+      expect(response).to redirect_to root_path
+
+      rock = Rock.last
+      expect(rock.name).to eq("Rock")
+    end
+  end
 end
