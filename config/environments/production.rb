2968dbe197b0bfa6ece96141f691d75be9361718
Karly Kizewski <kizewski.karly@gmail.com>
2019-12-11 04:28:56 +0000

    devise isntalled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'get-a-grip.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
